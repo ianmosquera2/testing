7775f48807194a4bb4f6f76660e9a2a4d2580dbc
Ian Mosquera <ianmosquera14@gmai.com>
2024-07-02 02:10:55 +0800

    update

---
+++ Practice/Practice/Pages/signup.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practice.Pages
+{
+    public class signupModel : PageModel
+    {
+        private readonly ILogger<signupModel> _logger;
+
+        public signupModel(ILogger<signupModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
---
+++ Practice/Practice/wwwroot/js/login.js
@@ -0,0 +1,4 @@
+﻿// JavaScript to handle navigation to signup page
+document.getElementById('signUpButton').addEventListener('click', function () {
+    window.location.href = 'signup.cshtml'; // Replace 'signup.cshtml' with your actual signup page path
+});
